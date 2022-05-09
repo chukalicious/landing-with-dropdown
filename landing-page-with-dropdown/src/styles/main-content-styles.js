4097ec7e4e8363cd3d446d6dd61131fbe1a5fc83
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-09 00:32:15 -0700

    created more styles for the main content area

--- landing-page-with-dropdown/src/styles/main-content-styles.js
+++ landing-page-with-dropdown/src/styles/main-content-styles.js
@@ -3,5 +3,21 @@ import styled from "styled-components";
 export const MainDiv = styled.div`
   width: 100%;
   margin: auto;
-  min-height: 75vh;
+  min-height: 83vh;
 `;
+
+export const h2Styles = {
+  fontWeight: "700",
+};
+
+export const h3Styles = {
+  textAlign: "justify",
+  fontWeight: "500",
+};
+
+export const textArea = {
+  display: "flex",
+  width: "90%",
+  margin: "auto",
+  justifyContent: "center",
+};
