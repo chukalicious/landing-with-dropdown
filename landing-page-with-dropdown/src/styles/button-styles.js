bb88a15708edc86493cf2348b4aff4237ae0eb27
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-08 23:58:59 -0700

    created action button styles

---
+++ landing-page-with-dropdown/src/styles/button-styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ActionButton = styled.div`
+  background-color: black;
+  color: white;
+  text-align: center;
+  padding: 0.75rem 0rem;
+  margin: auto;
+  font-size: 1.2rem;
+  font-weight: 700;
+  width: 35%;
+  border-radius: 15px;
+`;
