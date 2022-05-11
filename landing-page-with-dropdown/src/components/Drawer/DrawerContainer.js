5e65d4c149db04fffb34c9b60a8433ec4ee3d9a4
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-10 17:56:52 -0700

    same as previous commit. these 2 files should have been commited together

--- landing-page-with-dropdown/src/components/Drawer/DrawerContainer.js
+++ landing-page-with-dropdown/src/components/Drawer/DrawerContainer.js
@@ -1,3 +1,4 @@
+import DrawerListItems from "./DrawerListItems";
 import { drawerContainer } from "../../styles/drawer-styles";
 
 const DrawerContainer = (props) => {
@@ -7,40 +8,7 @@ const DrawerContainer = (props) => {
 
   return (
     <div style={drawerContainer}>
-      <div onClick={closeDrawer}>X</div>
-      <div>
-        <ul>
-          <div>
-            <li>
-              Features
-              <ul>
-                <li>To do</li>
-                <li>Calendar</li>
-                <li>Reminders</li>
-                <li>Planing</li>
-              </ul>
-            </li>
-          </div>
-          <div>
-            <li>
-              Company
-              <ul>
-                <li>History</li>
-                <li>Our Team</li>
-                <li>Blog</li>
-              </ul>
-            </li>
-          </div>
-          <div>
-            <li>Careers</li>
-          </div>
-          <div>
-            <li>About</li>
-          </div>
-        </ul>
-      </div>
-      <button>Login</button>
-      <button>Register</button>
+      <DrawerListItems closeDrawer={closeDrawer} />
     </div>
   );
 };
