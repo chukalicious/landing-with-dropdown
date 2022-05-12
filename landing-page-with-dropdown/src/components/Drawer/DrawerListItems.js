7d55335b41ae011dff3af913a1f705ebb43ee6fd
Katiuska Alicea <katiuska@Katiuskas-Air.lan>
2022-05-11 19:58:28 -0700

    because the out drawer items were nested inside the li with the open funtion, whenever we would click inside the ul the list would open or close causing undesirable effects. we want the list to remain open even after the user has made a selection among the links inside the drawer. to fix this I unnested the the li with the function, so that it does not affect the rest of the components

--- landing-page-with-dropdown/src/components/Drawer/DrawerListItems.js
+++ landing-page-with-dropdown/src/components/Drawer/DrawerListItems.js
@@ -10,13 +10,16 @@ import { drawerButtons } from "../../styles/button-styles";
 import { useState } from "react";
 
 const DrawerListItems = (props) => {
+  const closeDrawer = () => {
+    props.closeDrawer();
+  };
   console.log("DrawerListItems: props", props);
 
   const [openFeatures, setOpenFeatures] = useState(false);
   console.log("openFeatures", openFeatures);
   const [openCompany, setOpenCompany] = useState(false);
 
-  const openNestedFeatures = () => {
+  const openNestedFeatures = (e) => {
     setOpenFeatures(!openFeatures);
   };
 
@@ -24,48 +27,51 @@ const DrawerListItems = (props) => {
     setOpenCompany(!openCompany);
   };
 
-  const closeDrawer = () => {
-    props.closeDrawer();
+  const closeNestedCompany = () => {
+    setOpenFeatures(false);
   };
+
+  const closeNestedFeatures = () => {
+    setOpenFeatures(false);
+  };
+
   return (
     <div style={liContainer}>
       <div onClick={closeDrawer} style={xButton}>
         <p style={x}>X</p>
       </div>
-      <div>
-        <ul>
-          <div>
-            <li onClick={openNestedFeatures} style={outerDrawerLi}>
-              Features
-              {openFeatures ? (
-                <ul style={innerDrawerUl}>
-                  <li style={outerDrawerLi}>To do</li>
-                  <li style={outerDrawerLi}>Calendar</li>
-                  <li style={outerDrawerLi}>Reminders</li>
-                  <li style={outerDrawerLi}>Planing</li>
-                </ul>
-              ) : null}
-            </li>
-          </div>
-          <div>
-            <li onClick={openNestedCompany} style={outerDrawerLi}>
-              Company
-              {openCompany ? (
-                <ul style={innerDrawerUl}>
-                  <li style={outerDrawerLi}>History</li>
-                  <li style={outerDrawerLi}>Our Team</li>
-                  <li style={outerDrawerLi}>Blog</li>
-                </ul>
-              ) : null}
-            </li>
-          </div>
-          <div>
-            <li style={outerDrawerLi}>Careers</li>
-          </div>
-          <div>
-            <li style={outerDrawerLi}>About</li>
-          </div>
-        </ul>
+      <div style={{ border: "red 1px solid" }}>
+        <div>
+          <li onClick={openNestedFeatures} style={outerDrawerLi}>
+            Features{" "}
+          </li>
+          {openFeatures ? (
+            <ul onClick={() => closeNestedCompany} style={innerDrawerUl}>
+              <li style={outerDrawerLi}>To do</li>
+              <li style={outerDrawerLi}>Calendar</li>
+              <li style={outerDrawerLi}>Reminders</li>
+              <li style={outerDrawerLi}>Planing</li>
+            </ul>
+          ) : null}
+        </div>
+        <div>
+          <li onClick={openNestedCompany} style={outerDrawerLi}>
+            Company
+          </li>
+          {openCompany ? (
+            <ul style={innerDrawerUl}>
+              <li style={outerDrawerLi}>History</li>
+              <li style={outerDrawerLi}>Our Team</li>
+              <li style={outerDrawerLi}>Blog</li>
+            </ul>
+          ) : null}
+        </div>
+        <div>
+          <li style={outerDrawerLi}>Careers</li>
+        </div>
+        <div>
+          <li style={outerDrawerLi}>About</li>
+        </div>
       </div>
       <div style={buttonsWrapper}>
         <div style={drawerButtons}>Login</div>
